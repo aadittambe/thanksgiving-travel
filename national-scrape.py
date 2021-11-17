489e522b975fe9b805e8dcc4ee309346847bdd9b
Aadit Tambe <aadit.tambe@gmail.com>
2021-11-16 23:04:16 -0500

    x

--- national-scrape.py
+++ national-scrape.py
@@ -1,10 +1,3 @@
-import selenium
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 import requests
 from bs4 import BeautifulSoup
 from datetime import datetime
